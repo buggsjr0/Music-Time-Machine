274baed1a0a8fb1cf4462c80e6a8e2ceecf07554
buggsjr0 <125913670+buggsjr0@users.noreply.github.com>
2023-03-11 15:02:08 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Scraping Billboard 100
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.find_all("span", class_="chart-element__information__song")
+song_names = [song.getText() for song in song_names_spans]
+
+#Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=YOUR CLIENT ID,
+        client_secret=YOUR CLIENT SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
+
+#Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+#Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+#Adding songs found into the new playlist
+playlist =spotify_user.user_playlist_create(user=spotify_user.current_user()['id'],
+    name=f'{user_date} Billboard 100',
+    public=False,
+    collaborative=False,
+    description=f'{user_date} Billboard 100')
+
+
+spotify_user.playlist_add_items(playlist_id=playlist['id'], items=track_uris)
+
+
+#Acess the playlist via: Log in to Spotify, Scroll to bottom and hit "Web Player- Useful links",
+#Redirected to another pg, Playlist should be located on the bottom left
